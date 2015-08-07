f180ce1da80d2bf78198d9412fd95774745a8d12
noonhorse <teen@163.com>
2015-08-07 21:33:29 +0800

    add template

--- config/routes.js
+++ config/routes.js
@@ -4,3 +4,13 @@
 'use strict';
 
 /*Module dependencies*/
+
+var admin = require('../controller/admin'),
+	login = require('../controller/login');
+
+module.exports = function routes(app){
+	app.get('/login', login);
+
+	app.post('/login', login.loginin)
+
+}
---
+++ controller/login.js
@@ -0,0 +1,21 @@
+/**
+ * login controller
+ * @authors teen (hewendong@qiku.com/teen@163.com)
+ * @date    2015-08-07 14:41:57
+ * @version $Id$
+ */
+
+'use strict';
+
+
+var config = require('../config'),
+	path = require('path'),
+	fs = require('fs');
+
+
+var page = fs.readFileSync(path.join(__dirname, '../admin/views/index.html'), 'utf8')
+  .replace('{{version}}', config.version);
+
+module.exports = function* home(next) {
+  this.body = page;
+};
